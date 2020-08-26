6a1baf2135ac946acc296759c346116688dde815
Your Name <you@example.com>
2020-08-25 23:08:53 -0400

    new vuejs project

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,19 @@
 module.exports = {
   presets: [
-    '@vue/cli-plugin-babel/preset'
+    [
+      '@vue/app',
+      {
+        polyfills: ['es.promise', 'es.symbol']
+      }
+    ]
+  ],
+  plugins: [
+    [
+      'component',
+      {
+        libraryName: 'element-ui',
+        styleLibraryName: 'theme-chalk'
+      }
+    ]
   ]
-}
+};
