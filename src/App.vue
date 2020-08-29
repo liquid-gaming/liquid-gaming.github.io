61ff44464fe850cca0ae91a9c475247dc73bfab6
Jmaxwell <jwjmaxwell@outlook.com>
2020-08-28 21:39:32 -0400

    Footer is done

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,28 @@
 <template>
-  <div id="app">
-    <div>
-      <img alt="Vue logo" src="./assets/LiQ.png">
-      <Info/>
-    </div>    
-  </div>
+  <v-app>
+    <ToolBar />
+    <v-content style="background-color: white">
+      <!-- <HelloWorld /> -->
+      <router-view />
+    </v-content>
+    <Footer />
+  </v-app>
 </template>
 
 <script>
-import Info from "./components/Info.vue"
+import ToolBar from "./components/Toolbar";
+import Footer from "./components/Footer";
 
 export default {
   name: 'App',
+
   components: {
-    Info
+    ToolBar,
+    Footer
   },
-  data() {
-  },
-  mounted() {
-  }
+
+  data: () => ({
+    //
+  }),
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-app>
+    <v-content>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'home',
+  components: {
+  }
+}
+</script>
