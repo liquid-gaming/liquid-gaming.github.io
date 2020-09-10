3bffe3beec03cfa27ca9cfc4df9cbc8fb8a75ffb
jmaxwell <jmaxwell@ssmic.com>
2020-09-10 13:08:20 -0400

    again2

--- src/main.js
+++ src/main.js
@@ -8,11 +8,12 @@ import VueLazyload from 'vue-lazyload'
 Vue.use(VueLazyload)
 Vue.config.productionTip = false
 Vue.use(VueAnalytics, {
-  id: 'UA-135785528-2',
+  id: 'UA-177609629-1',
   router
 })
 
 new Vue({
+  router,
   vuetify,
   render: h => h(App)
 }).$mount('#app')
