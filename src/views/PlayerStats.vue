45f05055cd4499e1bd39960c247157a95431377d
jmaxwell <jmaxwell@ssmic.com>
2020-09-22 12:57:14 -0400

    just need to get the wording for player stats

--- src/views/PlayerStats.vue
+++ src/views/PlayerStats.vue
@@ -2,32 +2,43 @@
   <div class="home background-image">
     <v-container >
       <v-card style="border-radius: 20px;margin-top: 75px;padding:35px !important; background-color:rgb(34, 34, 34)">
-        <v-card>
-          <v-card-title>
-            Player Stats
-            <v-spacer></v-spacer>
-            <v-text-field
-              v-model="search"
-              append-icon="mdi-magnify"
-              label="Search"
-              single-line
-              hide-details
-            ></v-text-field>
-          </v-card-title>
-          <v-data-table
-            :headers="headers"
-            :items="itemsWithIndex"
-            :search="search"
-            item-key="name"
-            sort-by="kills"
-            sort-desc
-            :footer-props="{
-              showFirstLastPage: true,
-              firstIcon: 'mdi-arrow-collapse-left',
-              lastIcon: 'mdi-arrow-collapse-right',
-            }">
-            </v-data-table>
-        </v-card>
+        <v-row align="left" justify="left">
+          <v-col>
+            <div :class="{'display-3 font-weight-bold ': $vuetify.breakpoint.smAndUp, 'display-2 font-weight-bold': $vuetify.breakpoint.smAndDown}" :style="{'height:100px': $vuetify.breakpoint.smAndUp, 'height:200px': $vuetify.breakpoint.smAndDown}" >
+              Player Statistics 
+            </div>
+            <div class="subtitle-1" style="opacity: 0.75; margin:10px 0 40px 0">
+                Seeding is made to allow players to play fairly while we try to populate the server. We typically use Al Basrah Skirmish V1 to seed our servers. 
+                Please read the rules for seeding below and look at the image provided to see how we set up the map for seeding. The image also shows the locations 
+                of the FOBs and designated zone to fight over without crossing the colored lines of your associated team.
+            </div>
+            <v-card>
+              <v-card-title>
+                <v-text-field
+                  v-model="search"
+                  append-icon="mdi-magnify"
+                  label="Search"
+                  single-line
+                  hide-details
+                ></v-text-field>
+              </v-card-title>
+              <v-data-table
+                :headers="headers"
+                :items="itemsWithIndex"
+                :search="search"
+                item-key="name"
+                sort-by="Kills"
+                sort-desc
+                :footer-props="{
+                  showFirstLastPage: true,
+                  firstIcon: 'mdi-arrow-collapse-left',
+                  lastIcon: 'mdi-arrow-collapse-right',
+                  'items-per-page-options': [10, 25, 50, 100]
+                }">
+                </v-data-table>
+            </v-card>
+          </v-col>
+        </v-row>
       </v-card>
     </v-container>
   </div>
@@ -36,6 +47,7 @@
 <script>
 // @ is an alias to /src
 //Json files
+import topStats from "@/assets/json/top-stats.json"
 
 export default {
   name: "seeding",
@@ -51,32 +63,15 @@ export default {
           },
           {
             text: 'User Name',
-            value: 'name',
+            value: 'Name',
           },
-          { text: 'Kills', value: 'kills' },
-          { text: 'Deaths', value: 'deaths' },
-          { text: 'K/D', value: 'kd' },
-          { text: 'Revives', value: 'revives' },
-          { text: 'Wounds', value: 'wounds' },
+          { text: 'Kills', value: 'Kills' },
+          { text: 'Deaths', value: 'Deaths' },
+          { text: 'K/D', value: 'KD' },
+          { text: 'Wounds', value: 'Wounds' },
+          { text: 'Revives', value: 'Revives' },
         ],
-        players: [
-          {
-            "name": "123",
-            "wounds": "1196",
-            "kills": "929",
-            "deaths": "587",
-            "kd": "1.6",
-            "revives": "46"
-          },
-          {
-            "name": "test",
-            "wounds": "333",
-            "kills": "222",
-            "deaths": "444",
-            "kd": "6.9",
-            "revives": "12"
-          }      
-        ]
+        players: topStats.sort((a, b) => parseFloat(b.Kills) - parseFloat(a.Kills))
       }
   },
   computed: {
