3f1abdc84640a79924e160ae47c14146c83520fb
Jmaxwell <jwjmaxwell@outlook.com>
2020-09-22 23:17:48 -0400

    history

--- src/router/index.js
+++ src/router/index.js
@@ -7,7 +7,7 @@ import PlayerStats from '../views/PlayerStats.vue'
 Vue.use(VueRouter)
 
 export default new VueRouter({
-  mode: 'hash',
+  mode: 'history',
   base: process.env.BASE_URL,
   routes: [
     {
