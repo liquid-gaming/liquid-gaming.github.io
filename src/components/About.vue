99d9994cc4ec27a8a262909ee09071a5ef25c6bb
Joe <jmaxwell@ssmic.com>
2021-06-11 13:58:13 -0400

    fix discord link not displaying

--- src/components/About.vue
+++ src/components/About.vue
@@ -37,53 +37,51 @@
         <v-spacer></v-spacer>
         <template>
             <v-flex xs12 sm7 md6 lg4 xl3 just>
-                <v-hover v-slot:default="{ hover }">
-                    <v-card :elevation="hover ? 10 : 5" style="border-radius: 20px;">
-                        <v-card-text>
-                            <v-layout wrap my-5 style="margin: 0 !important; height: 400px; width:300px" >
-                                <v-flex justify-center class="mt-2">
-                                    <v-layout wrap>
-                                        <div>
-                                            <div :class="{'display-1 font-weight-bold ': $vuetify.breakpoint.smAndUp}">
-                                                Join Our Discord
-                                            </div>
-                                            <div v-if="$vuetify.breakpoint.smAndDown">
-                                                <iframe src="https://discordapp.com/widget?id=583882225012047873&theme=dark" width="275" height="400" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
-                                            </div>
-                                            <div v-else>
-                                                <iframe src="https://discordapp.com/widget?id=583882225012047873&theme=dark" width="350" height="400" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
-                                            </div>
+                <v-card style="border-radius: 20px;">
+                    <div>
+                        <v-layout wrap my-5 style="margin: 15px 0 0 0 !important; height: 400px; width:300px;">
+                            <v-flex justify-center class="mt-2">
+                                <v-layout wrap>
+                                    <div>
+                                        <div :class="{'display-1 font-weight-bold ': $vuetify.breakpoint.smAndUp}">
+                                            Join Our Discord
                                         </div>
-                                    </v-layout>
+                                        <div v-if="$vuetify.breakpoint.smAndDown">
+                                            <iframe src="https://discordapp.com/widget?id=583882225012047873&theme=dark" width="275" height="400" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
+                                        </div>
+                                        <div v-else>
+                                            <iframe src="https://discordapp.com/widget?id=583882225012047873&theme=dark" width="322" height="400" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
+                                        </div>
+                                    </div>
+                                </v-layout>
+                            </v-flex>
+                        </v-layout>
+                    </div>
+                </v-card> 
+                <div>
+                    <v-expansion-panels>
+                        <v-expansion-panel>
+                            <v-expansion-panel-header class="titleColour">Rules</v-expansion-panel-header>
+                            <v-expansion-panel-content>
+                                <template v-slot:actions>
+                                    <v-icon color="primary">gavel</v-icon>
+                                </template>
+                                <v-flex v-for="(rule, index) in discord.rules" :key="index">
+                                    <v-card class="card-container" style="margin:7px;background:#262626;">
+                                        <v-card-text class="px-4">
+                                            <v-card-title>
+                                                <h5 class="titleColour">{{index+1}}: {{rule.title}}</h5>
+                                            </v-card-title>
+                                            <v-card-subtitle v-if="rule.description != null">
+                                                <p>{{rule.description}}</p>
+                                            </v-card-subtitle>
+                                        </v-card-text>
+                                    </v-card>
                                 </v-flex>
-                            </v-layout>
-                        </v-card-text>
-                            <div>
-                                <v-expansion-panels>
-                                    <v-expansion-panel>
-                                        <v-expansion-panel-header class="titleColour">Rules</v-expansion-panel-header>
-                                        <v-expansion-panel-content>
-                                            <template v-slot:actions>
-                                                <v-icon color="primary">gavel</v-icon>
-                                            </template>
-                                            <v-flex v-for="(rule, index) in discord.rules" :key="index">
-                                                <v-card class="card-container" style="margin:7px;background:#262626;">
-                                                    <v-card-text class="px-4">
-                                                        <v-card-title>
-                                                            <h5 class="titleColour">{{index+1}}: {{rule.title}}</h5>
-                                                        </v-card-title>
-                                                        <v-card-subtitle v-if="rule.description != null">
-                                                            <p>{{rule.description}}</p>
-                                                        </v-card-subtitle>
-                                                    </v-card-text>
-                                                </v-card>
-                                            </v-flex>
-                                        </v-expansion-panel-content>
-                                    </v-expansion-panel>
-                                </v-expansion-panels>
-                            </div>
-                    </v-card>  
-                </v-hover>
+                            </v-expansion-panel-content>
+                        </v-expansion-panel>
+                    </v-expansion-panels>
+                </div>
             </v-flex>
         </template>
     </v-row>
