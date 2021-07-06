9fd0d07efe1488ede1aa04124ea9a27b803c52ef
Joe <jwjmaxwell@outlook.com>
2021-07-06 18:23:22 -0400

    cleaning up some links

--- src/constants/RouterLinks.js
+++ src/constants/RouterLinks.js
@@ -3,11 +3,25 @@ const MERCH = "https://shop.spreadshirt.com/liq-gaming/"
 const PATREON = "https://www.patreon.com/bePatron?u=38350579"
 const FEEDBACK_FORM = "https://forms.gle/hzZwuGSj2YHEBAQq8"
 const WEEKLY_VIDEO = "https://forms.gle/j1ZknbFRQ8ETUbQH6"
+const ANNOUNCEMENTS = "https://liqgaming.com/#/announcements"
+const MAP_ROTATION = "https://liqgaming.com/#/mapRotation"
+const LIQ_WEEKLY = "https://liqgaming.com/#/liqweekly"
+const PLAYER_STATS = "https://liqgaming.com/#/playerstats"
+const RULES = "https://liqgaming.com/#/rules"
+const SEEDINGS = "https://liqgaming.com/#/seeding"
+const SQUAD_MASTER = "https://liqgaming.com/#/squadMasters"
 
 export default {
   DISCORD: DISCORD,
   MERCH: MERCH,
   PATREON: PATREON,
   FEEDBACK_FORM: FEEDBACK_FORM,
-  WEEKLY_VIDEO: WEEKLY_VIDEO
+  ANNOUNCEMENTS: ANNOUNCEMENTS,
+  WEEKLY_VIDEO: WEEKLY_VIDEO,
+  MAP_ROTATION: MAP_ROTATION,
+  LIQ_WEEKLY: LIQ_WEEKLY,
+  PLAYER_STATS: PLAYER_STATS,
+  RULES: RULES,
+  SEEDINGS: SEEDINGS,
+  SQUAD_MASTER: SQUAD_MASTER,
 }
