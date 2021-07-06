b8eaa40807d52f3a5444e5a6322b82855a3de9d6
got2bhockey <got2bhockey@localhost>
2021-07-06 18:05:16 -0400

    fixed for real

--- src/constants/RouterLinks.js
+++ src/constants/RouterLinks.js
@@ -2,10 +2,12 @@ const DISCORD = "https://discord.gg/5WrXn4a"
 const MERCH = "https://shop.spreadshirt.com/liq-gaming/"
 const PATREON = "https://www.patreon.com/bePatron?u=38350579"
 const FEEDBACK_FORM = "https://forms.gle/hzZwuGSj2YHEBAQq8"
+const WEEKLY_VIDEO = "https://forms.gle/j1ZknbFRQ8ETUbQH6"
 
 export default {
   DISCORD: DISCORD,
   MERCH: MERCH,
   PATREON: PATREON,
-  FEEDBACK_FORM: FEEDBACK_FORM
+  FEEDBACK_FORM: FEEDBACK_FORM,
+  WEEKLY_VIDEO: WEEKLY_VIDEO
 }
