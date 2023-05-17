66bcb32b09ec40929cb729a7b0cfc4644acfea7a
LiQuid Gaming <liquidgamingcommunity@gmail.com>
2023-05-17 08:44:32 -0500

    New season wipe

--- src/views/PlayerStats.vue
+++ src/views/PlayerStats.vue
@@ -17,7 +17,7 @@
             </div>
             <div>Time Until Season Ends:</div>
             <div style="padding-right: 24px !important;">
-              <Countdown end="May 17, 2023"></Countdown>
+              <Countdown end="August 18, 2023"></Countdown>
             </div>
             
             <v-btn router to="/archivedplayerstats"  style="width: 98%;margin-top: 20px !important;" color="#1F2E29">
