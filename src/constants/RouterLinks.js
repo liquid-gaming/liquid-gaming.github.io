d00c1222feab980564418f7df5a9abec9fa530d9
TempusOwl <orion@syfusion.com>
2024-02-20 20:44:43 -0500

    New mortar calc

--- src/constants/RouterLinks.js
+++ src/constants/RouterLinks.js
@@ -3,7 +3,7 @@ const MERCH = "https://liq-gaming.myspreadshop.com"
 const PATREON = "https://www.patreon.com/bePatron?u=38350579"
 const FEEDBACK_FORM = "https://forms.gle/hzZwuGSj2YHEBAQq8"
 const WEEKLY_VIDEO = "https://forms.gle/j1ZknbFRQ8ETUbQH6"
-const MORTAR_CALCULATOR = "https://squadmortar.xyz/"
+const MORTAR_CALCULATOR = "https://squadcalc.app/"
 
 export default {
   DISCORD: DISCORD,
