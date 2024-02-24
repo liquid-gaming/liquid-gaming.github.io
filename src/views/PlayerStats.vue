bd9892e504fffd76a2df3380aa954fc53a586e5e
got2bhockey <got2bhockey@users.noreply.github.com>
2024-02-24 09:38:04 -0500

    Update PlayerStats.vue

--- src/views/PlayerStats.vue
+++ src/views/PlayerStats.vue
@@ -17,7 +17,7 @@
             </div>
             <div>Time Until Season Ends:</div>
             <div style="padding-right: 24px !important;">
-              <Countdown end="May 25th, 2024"></Countdown>
+              <Countdown end="May 25, 2024"></Countdown>
             </div>
             
             <v-btn router to="/archivedplayerstats"  style="width: 98%;margin-top: 20px !important;" color="#1F2E29">
