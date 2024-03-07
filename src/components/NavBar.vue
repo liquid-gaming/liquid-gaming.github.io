a0183328c87cc89e5cb9793523286cda46426856
orion <orion@syfusion.com>
2024-03-06 19:34:57 -0500

    Removed squad master (dead domain)

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -84,7 +84,6 @@ export default {
         { title: 'Player Stats', icon: 'leaderboard', route: '/playerstats' },
         { title: 'Squad Maps', icon: 'map', route: '/squadMaps' },
         { title: 'Mortar Calculator', icon: mdiCalculator, route: '/mortarCalculator' },
-        { title: 'Squad Masters', icon: mdiTrophyAward, route: '/squadMasters' },
         { title: 'Discord', icon: 'mdi-discord', route: '/discord' },
         { title: 'Merchandise', icon: 'shopping_cart', route: '/merch' },
         { title: 'Patreon', icon: 'mdi-patreon', route: '/patreon' }
