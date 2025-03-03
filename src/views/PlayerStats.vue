0e1b4530b97787d8250fe9fdff912592d63e1965
LiQuid Gaming <liquidgamingcommunity@gmail.com>
2025-03-03 09:05:04 -0600

    New season

--- src/views/PlayerStats.vue
+++ src/views/PlayerStats.vue
@@ -17,7 +17,7 @@
             </div>
             <div>Time Until Season Ends:</div>
             <div style="padding-right: 24px !important;">
-              <Countdown end="February 28, 2025"></Countdown>
+              <Countdown end="June 3, 2025"></Countdown>
             </div>
             
             <v-btn router to="/archivedplayerstats"  style="width: 98%;margin-top: 20px !important;" color="#1F2E29">
